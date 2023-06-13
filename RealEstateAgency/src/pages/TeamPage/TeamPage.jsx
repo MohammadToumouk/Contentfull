96e9e4e0e1612d4de6e2365b5881e61e08825507
MohammadToumouk <129755740+MohammadToumouk@users.noreply.github.com>
2023-06-13 21:09:56 +0200

    displayed the Agents and added basic styling

--- RealEstateAgency/src/pages/TeamPage/TeamPage.jsx
+++ RealEstateAgency/src/pages/TeamPage/TeamPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import ListingCards from '../../components/listingcards/listingCards'
+import './TeamPage.css'
 
-const TeamPage = () => {
+const TeamPage = ({propertyData}) => {
+  console.log(propertyData.sys)
+
   return (
-    <div>
-      <h1>Team Page</h1>
+    <>
+    <h1>Team Page</h1>
+
+    <div className='agentBigContainer'>
+      
+      
+      {propertyData?.map((agent) => (
+        <div className='agentContainer'>
+        <ListingCards
+        key={agent.sys.id}
+        id={agent.sys.id}
+        image={agent?.fields?.profilepic.fields.file.url}
+        propertyName={agent?.fields?.title}
+        
+      />
+
+      <p>{agent?.fields?.description}</p>
+      </div>
+      ))}
+      
+      
     </div>
+    </>
   )
 }
 
